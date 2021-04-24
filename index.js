6493bd4bc8beeb0fcb4ef69273381f55c222ef84
Batu Tarim <batutarim.online@gmail.com>
2021-04-24 11:45:47 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {Scroll()};
-
-function Scroll() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("scroll").style.width = scrolled + "%";
-}
