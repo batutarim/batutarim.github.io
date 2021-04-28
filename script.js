395757a2417f4c74865eab08cc29ed98991a27de
Batu Tarim <batutarim.online@gmail.com>
2021-04-28 19:08:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function Scroll() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("scroll").style.width = scrolled + "%";
 }
+
+if(typeof(EventSource) !== "undefined") {
+  var source = new EventSource("sse.php");
+  source.onmessage = function(event) {
+    document.getElementById("result").innerHTML += event.data + "<br>";
+  };
+} else {
+  document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
+}
