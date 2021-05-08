54975ca9dd64544933469f0d69211d3fa34b685e
Batu Tarim <batutarim.online@gmail.com>
2021-05-08 17:23:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ function Scroll() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("scroll").style.width = scrolled + "%";
 }
+
+console.log("%c  ", "background-image: url('https://batutarim.github.io/favicon.png'); background-repeat: no-repeat; background-size: 128px 128px; font-size: 128px")
+
+console.warn("You aren't supposed to be here >:/")
