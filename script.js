86ea5ffaf8728f8454aa889060ab2f1777e87f7b
Batu Tarim <batutarim.online@gmail.com>
2021-05-24 20:38:17 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {Scroll()};
+
+function Scroll() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("scroll").style.width = scrolled + "%";
+}
+
+console.error("you are not supposed to be here :/")
